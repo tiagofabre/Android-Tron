96c0979795b0b0fc759157f40625dcc7b37cf905
tiagofabre <tiagofabre@gmail.com>
2015-08-26 00:13:53 -0300

    NSD working

---
+++ course/src/com/kilobolt/robotgame/Snake.java
@@ -0,0 +1,19 @@
+package com.kilobolt.robotgame;
+
+import java.util.ArrayList;
+import java.util;
+
+import android.graphics.Rect;
+
+public class Snake {
+
+	final float velocity =  1;
+	public float lifetime = 0;
+	
+	
+	public void AddLifeTick()
+	{
+		lifetime++;
+	}
+	
+}
